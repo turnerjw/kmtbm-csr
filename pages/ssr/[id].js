638bf09a677c0d2d1c5dc8228ba26bf504176ae1
Justin Turner <jturner@spscommerce.com>
2020-11-10 15:06:16 -0500

    Clean up

---
+++ pages/ssr/[id].js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+const Pokemon = ({ data }) => {
+  return (
+    <div className="layout">
+      <div className="card">
+        <p className="text">
+          <b>{data.name.charAt(0).toUpperCase() + data.name.slice(1)}</b> #
+          {data.id}
+        </p>
+        <div className="image">
+          <Image
+            src={data.sprites.front_default}
+            width={100}
+            height={100}
+            priority
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${context.params.id}`
+  );
+  const data = await res.json();
+  return {
+    props: { data: data }, // will be passed to the page component as props
+  };
+}
+
+export default Pokemon;
---
+++ pages/ssr/how.js
@@ -0,0 +1,3 @@
+export default function How() {
+    return <img src="/CSR.svg"/>
+}
